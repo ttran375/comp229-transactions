3fb40aa313e6b2f80dfab315c209fe050fd7c13c
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-19 16:53:46 +0000

    Add transaction controller

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,104 +1,38 @@
-import Product from "../models/product.model.js";
-import extend from "lodash/extend.js";
+import Transaction from "../models/transaction.model.js";
+import Account from "../models/account.model.js";
 import errorHandler from "../helpers/dbErrorHandler.js";
-import formidable from "formidable";
-import fs from "fs";
 
-const create = (req, res, next) => {
-  let form = formidable({ keepExtensions: true });
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      return res.status(400).json({
-        message: "Image could not be uploaded",
-      });
-    }
-    Object.keys(fields).forEach((key) => (fields[key] = fields[key][0]));
-    Object.keys(files).forEach((key) => (files[key] = files[key][0]));
-    let product = new Product(fields);
-    product.shop = req.shop;
-    if (files.image) {
-      product.image.data = fs.readFileSync(files.image.filepath);
-      product.image.contentType = files.image.mimetype;
-    }
-    try {
-      let result = await product.save();
-      res.json(result);
-    } catch (err) {
-      return res.status(400).json({
-        error: errorHandler.getErrorMessage(err),
-      });
-    }
-  });
-};
-
-const productByID = async (req, res, next, id) => {
+const create = async (req, res) => {
+  const { amount, fromAccountNumber, toAccountNumber, description } = req.body;
   try {
-    let product = await Product.findById(id)
-      .populate("shop", "_id name")
-      .exec();
-    if (!product)
-      return res.status("400").json({
-        error: "Product not found",
-      });
-    req.product = product;
-    next();
-  } catch (err) {
-    return res.status("400").json({
-      error: "Could not retrieve product",
+    let fromAccount = await Account.findOne({
+      accountNumber: fromAccountNumber,
     });
-  }
-};
-
-const photo = (req, res, next) => {
-  if (req.product.image.data) {
-    res.set("Content-Type", req.product.image.contentType);
-    return res.send(req.product.image.data);
-  }
-  next();
-};
-
-const defaultPhoto = (req, res) => {
-  return null;
-};
-
-const read = (req, res) => {
-  req.product.image = undefined;
-  return res.json(req.product);
-};
-
-const update = (req, res) => {
-  let form = formidable({ keepExtensions: true });
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
+    let toAccount = await Account.findOne({
+      accountNumber: toAccountNumber,
+    });
+    if (!fromAccount || !toAccount) {
       return res.status(400).json({
-        message: "Photo could not be uploaded",
+        error: "One or both accounts not found",
       });
     }
-    Object.keys(fields).forEach((key) => (fields[key] = fields[key][0]));
-    Object.keys(files).forEach((key) => (files[key] = files[key][0]));
-    let product = req.product;
-    product = extend(product, fields);
-    product.updated = Date.now();
-    if (files.image) {
-      product.image.data = fs.readFileSync(files.image.filepath);
-      product.image.contentType = files.image.mimetype;
-    }
-    try {
-      let result = await product.save();
-      res.json(result);
-    } catch (err) {
+    if (fromAccount.balance < amount) {
       return res.status(400).json({
-        error: errorHandler.getErrorMessage(err),
+        error: "Insufficient funds in the source account",
       });
     }
-  });
-};
-
-const remove = async (req, res) => {
-  try {
-    let product = req.product;
-    let deletedProduct = await product.deleteOne({ _id: product._id });
-    res.json(deletedProduct);
+    let transaction = new Transaction({
+      amount,
+      fromAccount: fromAccount._id,
+      toAccount: toAccount._id,
+      description,
+    });
+    fromAccount.balance -= amount;
+    toAccount.balance += amount;
+    await transaction.save();
+    await fromAccount.save();
+    await toAccount.save();
+    res.status(200).json(transaction);
   } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err),
@@ -106,12 +40,18 @@ const remove = async (req, res) => {
   }
 };
 
-const listByShop = async (req, res) => {
+const listByAccount = async (req, res) => {
   try {
-    let products = await Product.find({ shop: req.shop._id })
-      .populate("shop", "_id name")
-      .select("-image");
-    res.json(products);
+    let transactions = await Transaction.find({
+      $or: [
+        { fromAccount: req.params.accountId },
+        { toAccount: req.params.accountId },
+      ],
+    })
+      .populate("fromAccount", "accountNumber")
+      .populate("toAccount", "accountNumber")
+      .exec();
+    res.json(transactions);
   } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err),
@@ -121,11 +61,5 @@ const listByShop = async (req, res) => {
 
 export default {
   create,
-  productByID,
-  photo,
-  defaultPhoto,
-  read,
-  update,
-  remove,
-  listByShop,
+  listByAccount,
 };
