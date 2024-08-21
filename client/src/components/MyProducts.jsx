79cbdae29be0d69aabe4dd732456493a4f7fec16
ttran375 <ttran375@my.centennialcollege.ca>
2024-08-21 18:49:45 -0400

    Add MyTransactions component

--- client/src/components/MyProducts.jsx
+++ client/src/components/MyProducts.jsx
@@ -1,152 +1,90 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import IconButton from "@material-ui/core/IconButton";
-import Icon from "@material-ui/core/Icon";
-import Edit from "@material-ui/icons/Edit";
+import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
 import Divider from "@material-ui/core/Divider";
-import { listByShop } from "../services/api-product.js";
-import DeleteProduct from "./DeleteProduct.jsx";
+import auth from "../services/auth-helper.js";
+import { listByAccount } from "../services/api-transaction.js";
 
 const useStyles = makeStyles((theme) => ({
-  products: {
-    padding: "24px",
-  },
-  addButton: {
-    float: "right",
-  },
-  leftIcon: {
-    marginRight: "8px",
+  root: {
+    maxWidth: 600,
+    margin: "auto",
+    padding: theme.spacing(3),
+    marginTop: theme.spacing(5),
   },
   title: {
     margin: theme.spacing(2),
     color: theme.palette.protectedTitle,
     fontSize: "1.2em",
   },
-  subheading: {
-    marginTop: theme.spacing(2),
-    color: theme.palette.openTitle,
-  },
-  cover: {
-    width: 110,
-    height: 100,
-    margin: "8px",
-  },
-  details: {
-    padding: "10px",
-  },
 }));
 
-export default function MyProducts(props) {
+export default function MyTransactions() {
   const classes = useStyles();
-  const [products, setProducts] = useState([]);
+  const { accountId } = useParams();
+  const [transactions, setTransactions] = useState([]);
+  const [error, setError] = useState("");
+  const jwt = auth.isAuthenticated();
   useEffect(() => {
     const abortController = new AbortController();
     const signal = abortController.signal;
-    listByShop(
-      {
-        shopId: props.shopId,
-      },
-      signal
-    ).then((data) => {
+    listByAccount({ accountId }, { t: jwt.token }, signal).then((data) => {
       if (data.error) {
-        console.log(data.error);
+        setError(data.error);
       } else {
-        setProducts(data);
+        setTransactions(data);
       }
     });
     return function cleanup() {
       abortController.abort();
     };
-  }, []);
-  const removeProduct = (product) => {
-    const updatedProducts = [...products];
-    const index = updatedProducts.indexOf(product);
-    updatedProducts.splice(index, 1);
-    setProducts(updatedProducts);
+  }, [accountId, jwt.token]);
+  const formatDate = (dateString) => {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    return new Date(dateString).toLocaleString("en-US", options);
   };
   return (
-    <Card className={classes.products}>
+    <Paper className={classes.root} elevation={4}>
       <Typography type="title" className={classes.title}>
-        Products
-        <span className={classes.addButton}>
-          <Link to={"/seller/" + props.shopId + "/products/new"}>
-            <Button color="primary" variant="contained">
-              <Icon className={classes.leftIcon}>add_box</Icon> New Product
-            </Button>
-          </Link>
-        </span>
+        Account History
       </Typography>
       <List dense>
-        {products.map((product, i) => {
-          return (
-            <span key={i}>
-              <ListItem>
-                <CardMedia
-                  className={classes.cover}
-                  image={
-                    "/api/product/image/" +
-                    product._id +
-                    "?" +
-                    new Date().getTime()
-                  }
-                  title={product.name}
-                />
-                <div className={classes.details}>
-                  <Typography
-                    type="headline"
-                    component="h2"
-                    color="primary"
-                    className={classes.productTitle}
-                  >
-                    {product.name}
-                  </Typography>
-                  <Typography
-                    type="subheading"
-                    component="h4"
-                    className={classes.subheading}
-                  >
-                    Quantity: {product.quantity} | Price: ${product.price}
-                  </Typography>
-                </div>
-                <ListItemSecondaryAction>
-                  <Link
-                    to={
-                      "/seller/" +
-                      product.shop._id +
-                      "/" +
-                      product._id +
-                      "/edit"
-                    }
-                  >
-                    <IconButton aria-label="Edit" color="primary">
-                      <Edit />
-                    </IconButton>
-                  </Link>
-                  <DeleteProduct
-                    product={product}
-                    shopId={props.shopId}
-                    onRemove={removeProduct}
-                  />
-                </ListItemSecondaryAction>
-              </ListItem>
-              <Divider />
-            </span>
-          );
-        })}
+        {transactions.map((transaction, i) => (
+          <span key={i}>
+            <ListItem>
+              <ListItemText
+                primary={`Amount: $${transaction.amount}`}
+                secondary={
+                  <>
+                    {`From: ${transaction.fromAccount.accountNumber} To: ${
+                      transaction.toAccount.accountNumber
+                    } - ${formatDate(transaction.date)}`}
+                    <br />
+                    {`Description: ${transaction.description}`}
+                  </>
+                }
+              />
+            </ListItem>
+            <Divider />
+          </span>
+        ))}
+        {transactions.length === 0 && !error && (
+          <Typography>No transactions found for this account.</Typography>
+        )}
+        {error && <Typography color="error">{error}</Typography>}
       </List>
-    </Card>
+    </Paper>
   );
 }
-
-MyProducts.propTypes = {
-  shopId: PropTypes.string.isRequired,
-};
