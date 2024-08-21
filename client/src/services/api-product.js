02288f67024b0ba0a9256398682699192b454d74
ttran375 <ttran375@my.centennialcollege.ca>
2024-08-21 18:35:03 -0400

    Add API transaction

--- client/src/services/api-product.js
+++ client/src/services/api-product.js
@@ -1,44 +1,30 @@
-const create = async (params, credentials, product) => {
+const create = async (credentials, transaction) => {
   try {
-    let response = await fetch("/api/products/by/" + params.shopId, {
+    let response = await fetch("/api/transaction", {
       method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify(transaction),
+    });
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const listByAccount = async (params, credentials, signal) => {
+  try {
+    let response = await fetch("/api/transactions/" + params.accountId, {
+      method: "GET",
+      signal: signal,
       headers: {
         Accept: "application/json",
         Authorization: "Bearer " + credentials.t,
       },
-      body: product,
     });
-    return response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const read = async (params, signal) => {
-  try {
-    let response = await fetch("/api/products/" + params.productId, {
-      method: "GET",
-      signal: signal,
-    });
-    return response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const update = async (params, credentials, product) => {
-  try {
-    let response = await fetch(
-      "/api/product/" + params.shopId + "/" + params.productId,
-      {
-        method: "PUT",
-        headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + credentials.t,
-        },
-        body: product,
-      }
-    );
     return response.json();
   } catch (err) {
     console.log(err);
@@ -47,28 +33,13 @@ const update = async (params, credentials, product) => {
 
 const remove = async (params, credentials) => {
   try {
-    let response = await fetch(
-      "/api/product/" + params.shopId + "/" + params.productId,
-      {
-        method: "DELETE",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + credentials.t,
-        },
-      }
-    );
-    return response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const listByShop = async (params, signal) => {
-  try {
-    let response = await fetch("/api/products/by/" + params.shopId, {
-      method: "GET",
-      signal: signal,
+    let response = await fetch("/api/transaction/" + params.transactionId, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
     });
     return response.json();
   } catch (err) {
@@ -76,62 +47,4 @@ const listByShop = async (params, signal) => {
   }
 };
 
-const listLatest = async (signal) => {
-  try {
-    let response = await fetch("/api/products/latest", {
-      method: "GET",
-      signal: signal,
-    });
-    return response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const listRelated = async (params, signal) => {
-  try {
-    let response = await fetch("/api/products/related/" + params.productId, {
-      method: "GET",
-      signal: signal,
-    });
-    return response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const listCategories = async (signal) => {
-  try {
-    let response = await fetch("/api/products/categories", {
-      method: "GET",
-      signal: signal,
-    });
-    return response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const list = async (params, signal) => {
-  const query = queryString.stringify(params);
-  try {
-    let response = await fetch("/api/products?" + query, {
-      method: "GET",
-    });
-    return response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export {
-  create,
-  read,
-  update,
-  remove,
-  listByShop,
-  listLatest,
-  listRelated,
-  listCategories,
-  list,
-};
+export { create, listByAccount, remove };
