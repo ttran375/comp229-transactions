493d66a6d31aa69cd367cb017678085e5ad22fe9
ttran375 <ttran375@my.centennialcollege.ca>
2024-08-21 18:31:53 -0400

    Add MyAccounts component

--- client/src/components/MyShops.jsx
+++ client/src/components/MyShops.jsx
@@ -3,20 +3,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
-import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
-import Icon from "@material-ui/core/Icon";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Edit from "@material-ui/icons/Edit";
 import Divider from "@material-ui/core/Divider";
-import auth from "../services/auth-helper";
-import { listByOwner } from "../services/api-shop.js";
+import auth from "../services/auth-helper.js";
+import { listByOwner } from "../services/api-account.js";
 import { Navigate, Link } from "react-router-dom";
-import DeleteShop from "./DeleteShop";
+import DeleteAccount from "./DeleteAccount.jsx";
 
 const useStyles = makeStyles((theme) => ({
   root: theme.mixins.gutters({
@@ -40,36 +37,32 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MyShops() {
+export default function MyAccounts() {
   const classes = useStyles();
-  const [shops, setShops] = useState([]);
+  const [accounts, setAccounts] = useState([]);
   const [redirectToSignin, setRedirectToSignin] = useState(false);
   const jwt = auth.isAuthenticated();
   useEffect(() => {
     const abortController = new AbortController();
     const signal = abortController.signal;
-    listByOwner(
-      {
-        userId: jwt.user._id,
-      },
-      { t: jwt.token },
-      signal
-    ).then((data) => {
-      if (data.error) {
-        setRedirectToSignin(true);
-      } else {
-        setShops(data);
+    listByOwner({ userId: jwt.user._id }, { t: jwt.token }, signal).then(
+      (data) => {
+        if (data.error) {
+          setRedirectToSignin(true);
+        } else {
+          setAccounts(data);
+        }
       }
-    });
+    );
     return function cleanup() {
       abortController.abort();
     };
   }, []);
-  const removeShop = (shop) => {
-    const updatedShops = [...shops];
-    const index = updatedShops.indexOf(shop);
-    updatedShops.splice(index, 1);
-    setShops(updatedShops);
+  const removeAccount = (account) => {
+    const updatedAccounts = [...accounts];
+    const index = updatedAccounts.indexOf(account);
+    updatedAccounts.splice(index, 1);
+    setAccounts(updatedAccounts);
   };
   if (redirectToSignin) {
     return <Navigate to="/signin" />;
@@ -78,50 +71,45 @@ export default function MyShops() {
     <div>
       <Paper className={classes.root} elevation={4}>
         <Typography type="title" className={classes.title}>
-          Your Shops
+          Your Accounts
           <span className={classes.addButton}>
-            <Link to="/seller/shop/new">
+            <Link to="/account/new">
               <Button color="primary" variant="contained">
-                <Icon className={classes.leftIcon}>add_box</Icon> New Shop
+                <Edit className={classes.leftIcon} /> New Account
               </Button>
             </Link>
           </span>
         </Typography>
         <List dense>
-          {shops.map((shop, i) => {
-            return (
-              <span key={i}>
-                <ListItem button>
-                  <ListItemAvatar>
-                    <Avatar
-                      src={
-                        "/api/shops/logo/" +
-                        shop._id +
-                        "?" +
-                        new Date().getTime()
-                      }
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={shop.name}
-                    secondary={shop.description}
-                  />
-                  {auth.isAuthenticated().user &&
-                    auth.isAuthenticated().user._id == shop.owner._id && (
-                      <ListItemSecondaryAction>
-                        <Link to={"/seller/shop/edit/" + shop._id}>
-                          <IconButton aria-label="Edit" color="primary">
-                            <Edit />
-                          </IconButton>
-                        </Link>
-                        <DeleteShop shop={shop} onRemove={removeShop} />
-                      </ListItemSecondaryAction>
-                    )}
-                </ListItem>
-                <Divider />
-              </span>
-            );
-          })}
+          {accounts.map((account, i) => (
+            <span key={i}>
+              <ListItem
+                button
+                component={Link}
+                to={`/account/${account._id}/transactions`}
+              >
+                <ListItemText
+                  primary={account.accountNumber}
+                  secondary={`Balance: $${account.balance}`}
+                />
+                {auth.isAuthenticated().user &&
+                  auth.isAuthenticated().user._id === account.owner._id && (
+                    <ListItemSecondaryAction>
+                      <Link to={`/account/edit/${account._id}`}>
+                        <IconButton aria-label="Edit" color="primary">
+                          <Edit />
+                        </IconButton>
+                      </Link>
+                      <DeleteAccount
+                        account={account}
+                        onRemove={removeAccount}
+                      />
+                    </ListItemSecondaryAction>
+                  )}
+              </ListItem>
+              <Divider />
+            </span>
+          ))}
         </List>
       </Paper>
     </div>
