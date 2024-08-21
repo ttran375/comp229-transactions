a34e573100d2317dc499455ba8adc4df1343ec7a
ttran375 <ttran375@my.centennialcollege.ca>
2024-08-21 18:40:16 -0400

    Add NewTransaction component

--- client/src/components/NewProduct.jsx
+++ client/src/components/NewProduct.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import FileUpload from "@material-ui/icons/AddPhotoAlternate";
-import auth from "../services/auth-helper.js";
 import Typography from "@material-ui/core/Typography";
-import Icon from "@material-ui/core/Icon";
 import { makeStyles } from "@material-ui/core/styles";
-import { create } from "../services/api-product.js";
-import { Link, Navigate, useParams } from "react-router-dom";
+import { create } from "../services/api-transaction.js";
+import auth from "../services/auth-helper.js";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -19,9 +17,6 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     marginTop: theme.spacing(5),
     paddingBottom: theme.spacing(2),
-  },
-  error: {
-    verticalAlign: "middle",
   },
   title: {
     marginTop: theme.spacing(2),
@@ -37,49 +32,33 @@ const useStyles = makeStyles((theme) => ({
     margin: "auto",
     marginBottom: theme.spacing(2),
   },
-  input: {
-    display: "none",
-  },
-  filename: {
-    marginLeft: "10px",
-  },
 }));
 
-export default function NewProduct() {
-  const params = useParams();
+export default function NewTransaction() {
   const classes = useStyles();
   const [values, setValues] = useState({
-    name: "",
+    amount: "",
+    fromAccountNumber: "",
+    toAccountNumber: "",
     description: "",
-    image: "",
-    category: "",
-    quantity: "",
-    price: "",
     redirect: false,
     error: "",
   });
   const jwt = auth.isAuthenticated();
+  if (!jwt.user.active) {
+    return <Navigate to="/" />;
+  }
   const handleChange = (name) => (event) => {
-    const value = name === "image" ? event.target.files[0] : event.target.value;
-    setValues({ ...values, [name]: value });
+    setValues({ ...values, [name]: event.target.value });
   };
   const clickSubmit = () => {
-    let productData = new FormData();
-    values.name && productData.append("name", values.name);
-    values.description && productData.append("description", values.description);
-    values.image && productData.append("image", values.image);
-    values.category && productData.append("category", values.category);
-    values.quantity && productData.append("quantity", values.quantity);
-    values.price && productData.append("price", values.price);
-    create(
-      {
-        shopId: params.shopId,
-      },
-      {
-        t: jwt.token,
-      },
-      productData
-    ).then((data) => {
+    const transaction = {
+      amount: values.amount || 0,
+      fromAccountNumber: values.fromAccountNumber || undefined,
+      toAccountNumber: values.toAccountNumber || undefined,
+      description: values.description || "",
+    };
+    create({ t: jwt.token }, transaction).then((data) => {
       if (data.error) {
         setValues({ ...values, error: data.error });
       } else {
@@ -88,108 +67,66 @@ export default function NewProduct() {
     });
   };
   if (values.redirect) {
-    return <Navigate to={"/seller/shop/edit/" + params.shopId} />;
+    return <Navigate to="/accounts" />;
   }
   return (
-    <div>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography type="headline" component="h2" className={classes.title}>
-            New Product
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography type="headline" component="h2" className={classes.title}>
+          New Transaction
+        </Typography>
+        <TextField
+          id="fromAccountNumber"
+          label="From Account Number"
+          className={classes.textField}
+          value={values.fromAccountNumber}
+          onChange={handleChange("fromAccountNumber")}
+          margin="normal"
+        />
+        <TextField
+          id="toAccountNumber"
+          label="To Account Number"
+          className={classes.textField}
+          value={values.toAccountNumber}
+          onChange={handleChange("toAccountNumber")}
+          margin="normal"
+        />
+        <TextField
+          id="amount"
+          label="Amount"
+          className={classes.textField}
+          value={values.amount}
+          onChange={handleChange("amount")}
+          type="number"
+          margin="normal"
+        />
+        <TextField
+          id="description"
+          label="Description"
+          className={classes.textField}
+          value={values.description}
+          onChange={handleChange("description")}
+          margin="normal"
+        />
+        {values.error && (
+          <Typography component="p" color="error">
+            {values.error}
           </Typography>
-          <br />
-          <input
-            accept="image/*"
-            onChange={handleChange("image")}
-            className={classes.input}
-            id="icon-button-file"
-            type="file"
-          />
-          <label htmlFor="icon-button-file">
-            <Button variant="contained" color="secondary" component="span">
-              Upload Photo
-              <FileUpload />
-            </Button>
-          </label>
-          <span className={classes.filename}>
-            {values.image ? values.image.name : ""}
-          </span>
-          <br />
-          <TextField
-            id="name"
-            label="Name"
-            className={classes.textField}
-            value={values.name}
-            onChange={handleChange("name")}
-            margin="normal"
-          />
-          <br />
-          <TextField
-            id="multiline-flexible"
-            label="Description"
-            multiline
-            rows="2"
-            value={values.description}
-            onChange={handleChange("description")}
-            className={classes.textField}
-            margin="normal"
-          />
-          <br />
-          <TextField
-            id="category"
-            label="Category"
-            className={classes.textField}
-            value={values.category}
-            onChange={handleChange("category")}
-            margin="normal"
-          />
-          <br />
-          <TextField
-            id="quantity"
-            label="Quantity"
-            className={classes.textField}
-            value={values.quantity}
-            onChange={handleChange("quantity")}
-            type="number"
-            margin="normal"
-          />
-          <br />
-          <TextField
-            id="price"
-            label="Price"
-            className={classes.textField}
-            value={values.price}
-            onChange={handleChange("price")}
-            type="number"
-            margin="normal"
-          />
-          <br />
-          {values.error && (
-            <Typography component="p" color="error">
-              <Icon color="error" className={classes.error}>
-                error
-              </Icon>
-              {values.error}
-            </Typography>
-          )}
-        </CardContent>
-        <CardActions>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={clickSubmit}
-            className={classes.submit}
-          >
-            Submit
-          </Button>
-          <Link
-            to={"/seller/shop/edit/" + params.shopId}
-            className={classes.submit}
-          >
-            <Button variant="contained">Cancel</Button>
-          </Link>
-        </CardActions>
-      </Card>
-    </div>
+        )}
+      </CardContent>
+      <CardActions>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={clickSubmit}
+          className={classes.submit}
+        >
+          Submit
+        </Button>
+        <Button variant="contained" className={classes.submit}>
+          Cancel
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
